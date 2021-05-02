213051730ed9a57d210bc9931e55f503ed7ab7bb
MJoynalAbedin <mdakibctg140@gmail.com>
2021-05-02 20:40:51 +0600

    failed in netlify

--- src/Components/Home/ProjectDetails/ProjectDetails.js
+++ src/Components/Home/ProjectDetails/ProjectDetails.js
@@ -5,7 +5,7 @@ const ProjectDetails = ({ project }) => {
     return (
         <div className="col-md-4 d-flex justify-content-center mb-5">
             <div className="card shadow" style={{width: '18rem'}}>
-                <img className="card-img-top" src={project.image} />
+                <img className="card-img-top" alt="" src={project.image} />
                 <div class="card-body text-center">
                     <h5 class="card-title text-uppercase">{project.name}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
